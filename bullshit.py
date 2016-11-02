9ea86214e75c22d62c0eae1495ae5858caa19324
Alex Carlin <carlin@ucdavis.edu>
2016-11-02 16:49:56 -0700

    Added a notebook to generate manifest.json for us

--- bullshit.py
+++ bullshit.py
@@ -2,10 +2,10 @@
 
 def bull_shit( p, params ):
 
-    selection_antibiotic = params[ 'selection_antibiotic' ]
-    mutants = params[ 'mutants' ]
+    selection_antibiotic = params[ 'selection_antibiotic' ] # this is for solid media only
+    mutants = eval( params[ 'mutants' ] ) # yeah, wow
 
-    # some bull shit recipes
+    # two bull shit recipes and 1 thermal cycle ramp
 
     recipe_kinase_mix = [
         ( 'PNK_buffer', 'rs16pc9rd5sg5d', 3 ),
@@ -14,54 +14,58 @@ def bull_shit( p, params ):
         ( '100mM_ATP', 'rs16pccshb6cb4', 1 ),
     ]
 
-    recipe_anneal_mix = [
-        ( 'DNA', params[ 'dna_aliquot'], ),
-        ( '')
-    ]
-
     recipe_polymerase_mix = [
-        ( '', '', ),
-
-            # reagents = {"buffer": {"resource_id": 'rs17sh5rzz79ct', "reagent_ratio": 0.6},
-            #             "t4ligase": {"resource_id": 'rs16pc8krr6ag7', "reagent_ratio": 0.4},
-            #             "t7polymerase": {"resource_id": 'rs16pca2urcz74', "reagent_ratio": 0.4},
-            #             "dntp": {"resource_id": 'rs16pcb542c5rd', "reagent_ratio": 0.4}
-            #             }
+        ( 'T4_ligase_buffer_10X', 'rs17sh5rzz79ct', 0.6 ),
+        ( 'T4_ligase', 'rs16pc8krr6ag7', 0.4 ),
+        ( 'T7_pol', 'rs16pca2urcz74', 0.4 ),
+        ( 'dNTPs', 'rs16pcb542c5rd', 0.4 ),
     ]
 
-    # synthesize oligos into 96-well plate
-    # minimum 24 mutants
+    ramp = [ { 'duration': '60:second', 'temperature': '{}:celsius'.format( 95 - i ) } for i in range( 70 ) ]
+
+    # wrangle the fucking oligos
 
     # synthetize oligos into tubes
     # and reformat into plate
     # expensive? more expensive then ordering dummy oligos?
+    oligo_pt = p.ref( 'oligo_pt', None, '96-pcr', storage='cold_20' )
+    oligos = []
+    i = 0
+    for mutant, oligo_list in mutants.items():
+        for oligo in oligo_list:
+            my_tube = p.ref( 'oligo_{}'.format( i ), None, 'micro-2.0', storage='cold_20' )
+            i += 1
+            oligos.append( { 'oligo_label': oligo, 'sequence': oligo, 'destination': oligo_pt.well( i ), 'scale': '25nm' } )
+    p.oligosynthesize( oligos )
 
+    # save for later, will use these a lot
     n_oligos = len( oligos )
+    n_mutants = len( mutants )
+    oligo_indexes = list( range( n_oligos ) )
+    mutant_indexes = list( range( n_mutants ) )
 
     # provision kinase mix
     # into micro tube
     kin_mx = p.ref( 'kinase_mix', None, 'micro-2.0', discard=True )
-    for ts_name, ts_id, vol in recipe_kinase_mix:
-        p.provision( kin_mx.well(0), ts_id, '{}:microliter'.format( vol * n_oligos ) )
+    for ts_name, ts_id, vol in recipe_kinase_mix: # idiom for provisioning from recipes
+        p.provision( ts_id, kin_mx.well(0), '{}:microliter'.format( vol * n_oligos ) )
 
     # new plate for kinasing?
-    kin_pt = p.ref( 'kinase_plate', None, 'micro-2.0', discard=True )
-    wells_to_kinase = range( n_oligos )
-    p.transfer( kin_mx, wells_to_kinase, '23:microliter' )
+    kin_pt = p.ref( 'kinase_plate', None, '96-pcr', discard=True )
+    p.distribute( kin_mx.well(0), kin_pt.wells( oligo_indexes ), '23:microliter' )
     p.stamp( oligo_pt, kin_pt, '7:microliter' )
-
     p.seal( kin_pt )
     p.incubate( kin_pt, 'warm_37', '1:hour' )
 
     # dilute and mix together oligos
-    p.unseal( kinase_pt )
+    p.unseal( kin_pt )
     dil_pt = p.ref( 'dilute_plate', None, '96-flat', discard=True )
     p.dispense_full_plate( dil_pt, 'water', '200:microliter' )
     #p.stamp( kin_pt, dil_pt, '2:microliter' ) for singles only!
     # for multiples, we have to do
-    for mutant_label, oligos in mutants:
-        for oligo_index in oligos:
-            p.transfer( oligo_pt.well( ol_index ), dilute_pt.well( mutant_index ), '2:microliter' )
+    for mutant_index, __ in enumerate( mutants ):
+        for oligo_index, __ in enumerate( oligos ):
+            p.transfer( oligo_pt.well( oligo_index ), dil_pt.well( mutant_index ), '2:microliter' )
             # how annoying
     p.cover( dil_pt )
     p.spin( dil_pt, '2000:g', '10:second' )
@@ -70,50 +74,57 @@ def bull_shit( p, params ):
     # make anneal master mix
     # create yet another new plate
     ann_pt = p.ref( 'anneal_plate', None, '384-pcr', storage='cold_20' )
-    p.transfer( params[ 'dna_aliquot' ].well( 0 ), ann_pt.wells( range( n_mutants ) ), '2:microliter' )
+    p.transfer( params[ 'dna_aliquot' ], ann_pt.wells( mutant_indexes ), '2:microliter' )
     p.stamp( dil_pt, ann_pt, '2:microliter', mix_after=True )
     p.seal( ann_pt )
     p.spin( ann_pt, '2000:g', '10:second' )
-    ramp = # a thermocylce ramp
     p.thermocycle( ann_pt, [{ 'cycles': 1, 'steps': ramp, 'volume': '5:microliter' }] )
 
     # polymerize
     p.unseal( ann_pt )
     p_mx = p.ref( 'polymerize_mix', None, 'micro-2.0', discard=True )
     for ts_name, ts_id, vol in recipe_kinase_mix:
-        p.provision( p_mx.well(0), ts_id, '{}:microliter'.format( vol * n_oligos ) )
-    p.transfer( p_mx.well(0), ann_pt.wells( range( n_mutants ) ), '2:microliter' )
+        p.provision( ts_id, p_mx.well(0), '{}:microliter'.format( vol * n_oligos ) )
+    p.transfer( p_mx.well(0), ann_pt.wells( mutant_indexes ), '2:microliter' )
     p.incubate( ann_pt, 'ambient', '90:minute' )
 
     # transform
     tr_pt = p.ref( "transformation_plate", None, "96-pcr", discard=True )
     p.incubate( tr_pt, "cold_20", "10:minute" )
-    p.provision( "rs16pbjc4r7vvz", tr_pt.wells( range( n_mutants ) ), "10:microliter")
+    p.provision( "rs16pbjc4r7vvz", tr_pt.wells( mutant_indexes ), "10:microliter")
     p.stamp( ann_pt, tr_pt, '2:microliter' )
     p.seal( tr_pt )
     p.incubate( tr_pt, 'cold_4', '20:minute' )
     p.unseal( tr_pt )
-    p.dispense_full_plate( transformation_plate, 'soc', '40:microliter' )
+    p.dispense_full_plate( tr_pt, 'soc', '40:microliter' )
     p.seal( tr_pt )
     p.incubate( tr_pt, 'warm_37', '60:minute', shaking=True )
 
     # plate
-    agar_plates = []
-    agar_wells = WellGroup([])
-    for well in range(0, len(transformation_wells), 6):
-        agar_plate = ref_kit_container(p,
-                                       "agar-%s_%s" % (len(agar_plates), printdatetime(time=False)),
-                                       "6-flat",
-                                       return_agar_plates(6)[params['growth_media']],
-                                       discard=False, store='cold_4')
-        agar_plates.append(agar_plate)
-        for i, w in enumerate(transformation_wells[well:well + 6]):
-            p.spread(w, agar_plate.well(i), "100:microliter")
-            agar_wells.append(agar_plate.well(i).set_name(w.name))
+    # possible selection antibiotics:
+    choices = {
+        "kanamycin": "ki17rs7j799zc2",
+        "ampicillin": "ki17sbb845ssx9"
+    }
 
-    for agar_p in agar_plates:
-        p.incubate( agar_p, 'warm_37', '12:hour' )
-        p.image_plate( agar_p, mode='top', dataref=agar_p.name )
+    # Transcriptic provisioning bull shit
+    # from autoprotocol.container import Container
+    # from autoprotocol.protocol import Ref
+    #
+    # agar_plates = []
+    # for plate_index in range( n_mutants, 6 ):
+    #     pt_name = '{}_{}'
+    #     kit_item = Container( None, p.container_type( '6-flat' ) )
+    #     the_pt = Ref( pt_name, { "reserve": choices[ selection_antibiotic ], "store": { "where": 'cold_4' }}, kit_item )
+    #     p.refs[ pt_name ] = the_pt # dumb
+    #
+    #     agar_plates.append( the_pt )
+    #     for i, w in enumerate( mutant_indexes[ plate_index : plate_index + 6 ] ):
+    #         p.spread( w, agar_plate.well(i), '100:microliter' )
+    #
+    # for agar_p in agar_plates:
+    #     p.incubate( agar_p, 'warm_37', '12:hour' )
+    #     p.image_plate( agar_p, mode='top', dataref=agar_p.name )
 
 if __name__ == '__main__':
     from autoprotocol.harness import run
